bc2a9d2788b33d4914747fcdc2decd2a7aa3420f
zw <1272314653@qq.com>
2021-06-21 20:02:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zw
+ * @create 2021-06-21 19:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
