a4df8b39ab93c70cf32e9a095d5c566481716722
zw <1272314653@qq.com>
2021-06-21 20:27:06 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello.github3");
         System.out.println("hello.github4");
         System.out.println("hot-fix text");
+        System.out.println("push text");
     }
 }
